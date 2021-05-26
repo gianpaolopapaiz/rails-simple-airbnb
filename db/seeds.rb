44d09707eaa9fe09941de3f526b3598c921c1f3f
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-05-26 16:06:43 -0300

    in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Starting seed..'
+Flat.destroy_all
+
+10.times do |i|
+	flat = Flat.new
+	flat.name = "Flat #{i + 1}"
+	flat.address = "Rua João #{i + 1}, São Paulo"
+    flat.description = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+	flat.price_per_night = 10 * (i + 1)
+	flat.number_of_guests = i + 1
+	flat.save
+end
+puts 'Seed completed!'
